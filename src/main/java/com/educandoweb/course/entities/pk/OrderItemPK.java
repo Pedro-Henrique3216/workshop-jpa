dc61b898f524ac06b4370fb50ada7bd955c98879
Pedro Henrique <ph6704938@gmail.com>
2023-12-20 11:10:44 -0300

    OrderItem, many-to-many association with extra attributes - PART 2

--- src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
+++ src/main/java/com/educandoweb/course/entities/pk/OrderItemPK.java
@@ -13,7 +13,7 @@ import java.util.Objects;
 @Embeddable
 public class OrderItemPK implements Serializable {
     @Serial
-    private static final Long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     @ManyToOne
     @JoinColumn(name = "order_id")
     private Order order;
